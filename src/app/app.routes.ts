44c02e4ee0cb6d974bb28d19414992a420b56f69
Yurii Korolev <korolev-2512@mail.ru>
2026-02-22 02:49:14 +0300

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from '../pages/home.component';
-import { MenuComponent } from '../pages/menu.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'menu', component: MenuComponent },
-];
