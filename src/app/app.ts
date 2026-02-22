c7ab530daf931a5b7d5cd7d9550e9d7c44d49d17
Yurii Korolev <korolev-2512@mail.ru>
2026-02-22 18:01:55 +0300

    feature: добавлен адаптив и оптимизация изображений

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NzLayoutModule, NzMenuModule],
+  imports: [NgOptimizedImage, RouterOutlet, RouterLink, RouterLinkActive, NzLayoutModule, NzMenuModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush
